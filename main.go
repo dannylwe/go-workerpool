1c2c7105fc3c8b9de96425e4044ce3eef66fdd05
Daniel Lwetabe <acehanks@gmail.com>
2020-01-06 11:43:39 +0300

    workers complete

--- main.go
+++ main.go
@@ -14,16 +14,16 @@ type JobResult struct {
 	Output string
 }
 
-func worker(id int, wg *sync.WaitGroup, jobChannel <-chan Job) {
+func worker(id int, wg *sync.WaitGroup, jobChannel <-chan Job, resutlChannel chan JobResult) {
 	defer wg.Done()
 	for job := range jobChannel {
-		doSomething(id, job)
+		resutlChannel <- doSomething(id, job)
 	}
 }
 
 func doSomething(workerID int, job Job) JobResult {
 	fmt.Printf("Worker #%d Running job #%d\n", workerID, job.Id)
-	time.Sleep(time.Second * 1)
+	time.Sleep(time.Millisecond * 500)
 	return JobResult{Output: "success"}
 }
 
@@ -31,6 +31,7 @@ func main() {
 	start := time.Now()
 	var jobs []Job
 
+	// create jobs
 	for i := 0; i < 100; i++ {
 		jobs = append(jobs, Job{Id: i})
 	}
@@ -39,12 +40,13 @@ func main() {
 	var (
 		wg         sync.WaitGroup
 		jobChannel = make(chan Job)
+		jobResultChannel = make(chan JobResult, len(jobs))
 	)
 	wg.Add(NumberOfWorkers)
 
 	// start workers
 	for j := 0; j < NumberOfWorkers; j++ {
-		go worker(j, &wg, jobChannel)
+		go worker(j, &wg, jobChannel, jobResultChannel)
 	}
 
 	// send jobs to worker
@@ -54,5 +56,12 @@ func main() {
 
 	close(jobChannel)
 	wg.Wait()
+	close(jobResultChannel)
+
+	var jobResults []JobResult
+	for result := range jobResultChannel {
+		jobResults = append(jobResults, result)
+	}
+	fmt.Println(jobResults)
 	fmt.Printf("Took %s\n", time.Since(start))
 }
