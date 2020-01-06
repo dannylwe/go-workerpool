9a26b20fcf8cd7867a80960307763f50f32c7eea
Daniel Lwetabe <acehanks@gmail.com>
2020-01-06 11:31:44 +0300

    workers

--- main.go
+++ main.go
@@ -1,23 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"sync"
 	"time"
 )
 
+type Job struct {
+	Id int
+}
+
+type JobResult struct {
+	Output string
+}
+
+func worker(id int, wg *sync.WaitGroup, jobChannel <-chan Job) {
+	defer wg.Done()
+	for job := range jobChannel {
+		doSomething(id, job)
+	}
+}
+
+func doSomething(workerID int, job Job) JobResult {
+	fmt.Printf("Worker #%d Running job #%d\n", workerID, job.Id)
+	time.Sleep(time.Second * 1)
+	return JobResult{Output: "success"}
+}
+
 func main() {
-	// program waits prints "hello" and waits three seconds for world
-	dearChannel := make(chan string)
-	worldChannel := make(chan string)
+	start := time.Now()
+	var jobs []Job
 
-	go func() {
-		time.Sleep(time.Second * 3)
-		worldChannel <- "world"
-	}()
+	for i := 0; i < 100; i++ {
+		jobs = append(jobs, Job{Id: i})
+	}
 
-	go func() {
-		time.Sleep(time.Second * 2)
-		dearChannel <- "dear"
-	}()
+	const NumberOfWorkers = 10
+	var (
+		wg         sync.WaitGroup
+		jobChannel = make(chan Job)
+	)
+	wg.Add(NumberOfWorkers)
 
-	println("hello", <-dearChannel, <-worldChannel)
+	// start workers
+	for j := 0; j < NumberOfWorkers; j++ {
+		go worker(j, &wg, jobChannel)
+	}
+
+	// send jobs to worker
+	for _, job := range jobs {
+		jobChannel <- job
+	}
+
+	close(jobChannel)
+	wg.Wait()
+	fmt.Printf("Took %s\n", time.Since(start))
 }
